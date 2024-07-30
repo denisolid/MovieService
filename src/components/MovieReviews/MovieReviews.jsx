f644527c05db0e605e2e1e2af7d4997fe00bc160
denisolid <dkolisnichienko321@gmail.com>
2024-07-30 23:19:59 +0000

    reviews styles

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchMovieReviews } from "../../services/api";
+import s from "./MovieReviews.module.css";
 
 const MovieReviews = () => {
   const { movieId } = useParams();
@@ -20,11 +21,11 @@ const MovieReviews = () => {
   }, [movieId]);
 
   return (
-    <div>
+    <div className={s.movieReviews}>
       <h3>Reviews</h3>
       <ul>
         {reviews.map((review) => (
-          <li key={review.id}>
+          <li key={review.id} className={s.review}>
             <h4>{review.author}</h4>
             <p>{review.content}</p>
           </li>
