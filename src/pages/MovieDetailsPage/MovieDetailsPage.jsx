4375a6d70c58a182f794deb0c4f260fa33e0b356
denisolid <dkolisnichienko321@gmail.com>
2024-07-31 20:20:43 +0000

    other changes

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -3,6 +3,7 @@ import { useParams, Routes, Route, Link, useLocation } from "react-router-dom";
 import { fetchMovieDetails } from "../../services/api";
 import MovieCast from "../../components/MovieCast/MovieCast";
 import MovieReviews from "../../components/MovieReviews/MovieReviews";
+import GoBackBtn from "../../components/GoBackBtn/GoBackBtn";
 import s from "./MovieDetailsPage.module.css";
 
 const MovieDetailsPage = () => {
@@ -25,15 +26,29 @@ const MovieDetailsPage = () => {
   }, [movieId]);
 
   return (
-    <div className={s.movieDetailsPage}>
+    <div>
       {movie && (
         <>
-          <h1>{movie.title}</h1>
-          <p className={s.overview}>{movie.overview}</p>
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={movie.title}
-          />
+          <GoBackBtn path={goBack.current}>Back to movies</GoBackBtn>
+          <div className={s.movieDetailsPage}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+            />
+            <div className={s.info}>
+              <h1 className={s.title}>{movie.title}</h1>
+              <h3 className={s.title}>Overview:</h3>
+              <p className={s.overview}>{movie.overview}</p>
+              <p>User score: {(movie.vote_average * 10).toFixed(0)}%</p>
+              <h3 className={s.title}>Genres:</h3>
+              <ul className={s.genres}>
+                {movie.genres.map((e) => (
+                  <li key={e.id}>{e.name}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
           <nav className={s.links}>
             <Link to={`/movies/${movieId}/cast`}>Cast</Link>
             <Link to={`/movies/${movieId}/reviews`}>Reviews</Link>
