2a60d9301cb4f0d00afa03a82e3a71783b550212
denisolid <dkolisnichienko321@gmail.com>
2024-07-31 22:37:31 +0000

    Reviews done and styled

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -2,18 +2,24 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchMovieReviews } from "../../services/api";
 import s from "./MovieReviews.module.css";
+import Loader from "../../components/Loader/Loader";
 
 const MovieReviews = () => {
   const { movieId } = useParams();
   const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchReviews = async () => {
+      setLoading(true);
       try {
         const response = await fetchMovieReviews(movieId);
         setReviews(response.data.results);
       } catch (error) {
-        console.error(error);
+        setError(error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -22,18 +28,28 @@ const MovieReviews = () => {
 
   return (
     <div className={s.movieReviews}>
-      <h3>Reviews</h3>
-      {reviews.length > 0 ? (
-        <ul>
-          {reviews.map((review) => (
-            <li key={review.id} className={s.review}>
-              <h4 className={s.title}>{review.author}</h4>
-              <p>{review.content}</p>
-            </li>
-          ))}
-        </ul>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <p>Something went wrong...</p>
       ) : (
-        <p className={s.noReviews}>Do not have any reviews for this movie</p>
+        <>
+          <h3>Reviews</h3>
+          {reviews.length > 0 ? (
+            <ul>
+              {reviews.map((review) => (
+                <li key={review.id} className={s.review}>
+                  <h4 className={s.title}>{review.author}</h4>
+                  <p>{review.content}</p>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className={s.noReviews}>
+              Do not have any reviews for this movie
+            </p>
+          )}
+        </>
       )}
     </div>
   );
