887b329786e99d0fe3bd43a33e6188777e9385ee
denisolid <dkolisnichienko321@gmail.com>
2024-07-31 22:36:19 +0000

    Loader done and styled

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,7 @@
 import { TailSpin } from "react-loader-spinner";
 
 const Loader = () => {
-  return <TailSpin color="#00BFFF" height={80} width={80} />;
+  return <TailSpin color="#f40612" height={40} width={40} />;
 };
 
 export default Loader;
