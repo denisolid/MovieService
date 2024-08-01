0fb1124fb4e01ddfbd3deb90669ea0b076dcef85
denisolid <dkolisnichienko321@gmail.com>
2024-08-01 18:33:43 +0000

    updatet routes for cast reviews in app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import { Navigation } from "./Navigation/Navigation";
 import { lazy, Suspense } from "react";
+import MovieCast from "./MovieCast/MovieCast";
+import MovieReviews from "./MovieReviews/MovieReviews";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 
 const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
 const MoviesPage = lazy(() => import("../pages/MoviesPage/MoviesPage"));
 const MovieDetailsPage = lazy(() =>
   import("../pages/MovieDetailsPage/MovieDetailsPage")
 );
-const NotFoundPage = lazy(() =>
-  import("../pages/MovieDetailsPage/MovieDetailsPage")
-);
+// const NotFoundPage = lazy(() => import("../pages/NotFoundPage/NotFoundPage"));
 
 function App() {
   return (
@@ -19,7 +20,10 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:movieId/*" element={<MovieDetailsPage />} />
+          <Route path="/movies/:movieId/*" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
