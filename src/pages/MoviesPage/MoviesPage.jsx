b6e83e2ac5112ff8435451154fd914edf112692f
denisolid <dkolisnichienko321@gmail.com>
2024-08-01 20:03:11 +0000

    movies page updated

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -3,39 +3,44 @@ import { useSearchParams } from "react-router-dom";
 import { searchMovies } from "../../services/api";
 import MovieList from "../../components/MovieList/MovieList";
 import Loader from "../../components/Loader/Loader";
+import Pagination from "../../components/Pagination/Pagination";
 import s from "./MoviesPage.module.css";
 
 const MoviesPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [query, setQuery] = useState(searchParams.get("query") || "");
+  // const [query, setQuery] = useState(searchParams.get("query") || "");
+  const query = searchParams.get("query") || "";
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const page = Number(searchParams.get("page")) || 1;
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
-    if (query === "") {
-      setMovies([]);
-      return;
+    if (query) {
+      const fetchMovies = async () => {
+        try {
+          setLoading(true);
+          const response = await searchMovies(query, page);
+          setMovies(response.data.results);
+          setTotalPages(response.data.total_pages);
+        } catch (error) {
+          setError(error);
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchMovies();
     }
-
-    const delayDebounceFn = setTimeout(async () => {
-      setLoading(true);
-      try {
-        const response = await searchMovies(query);
-        setMovies(response.data.results);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    }, 500);
-
-    return () => clearTimeout(delayDebounceFn);
-  }, [query]);
+  }, [query, page]);
 
   const handleQueryChange = (e) => {
-    setQuery(e.target.value);
-    setSearchParams({ query: e.target.value });
+    setSearchParams({ query: e.target.value, page: 1 });
+  };
+
+  const handlePageChange = (newPage) => {
+    setSearchParams({ query, page: newPage });
   };
 
   return (
@@ -51,7 +56,12 @@ const MoviesPage = () => {
       </div>
       {loading && <Loader />}
       {error && <p>Something went wrong...</p>}
-      {!loading && <MovieList movies={movies} state={{ query }} />}
+      {!loading && <MovieList movies={movies} />}
+      <Pagination
+        currentPage={page}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 };
