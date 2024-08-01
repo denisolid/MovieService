f73a25ef314c6feda9036dcd2cf8c71308dc1bf7
denisolid <dkolisnichienko321@gmail.com>
2024-08-01 20:03:23 +0000

    hp updated

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 import { fetchTrends } from "../../services/api";
-
+import Pagination from "../../components/Pagination/Pagination";
 import Loader from "../../components/Loader/Loader";
 import MoviesList from "../../components/MovieList/MovieList";
 
@@ -9,14 +9,17 @@ const HomePage = () => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true);
         setError(null);
-        const res = await fetchTrends();
+        const res = await fetchTrends(page);
         setMovies(res.data.results);
+        setTotalPages(res.data.total_pages);
       } catch (error) {
         setError(error);
       } finally {
@@ -25,7 +28,11 @@ const HomePage = () => {
     };
 
     fetchData();
-  }, []);
+  }, [page]);
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
 
   return (
     <>
@@ -33,6 +40,11 @@ const HomePage = () => {
       {error && <p>Something wrong...</p>}
 
       <MoviesList movies={movies}>Tredings movies :</MoviesList>
+      <Pagination
+        currentPage={page}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
     </>
   );
 };
